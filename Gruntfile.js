2fe186e2ce0df16fdf2d0b30fd5e3a90f74d45e0
Jerry Chang <therealjerrycan@gmail.com>
2015-10-10 14:43:56 -0700

    updated the package.json file and ignored the node_modules files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,6 @@
 module.exports = function(grunt){
     
-    grunt.initConfig({
-        
-        concat: {
-            js: {
-              src: ['js/intro.js', 'src/project.js', 'src/outro.js'],
-              dest: 'build/js/main.js',
-            },
-            css: {
-              src: ['css/main.css', 'css/theme.css'],
-              dest: 'build/css/styles.css',
-            },
-        },
-
+    grunt.initConfig({    
         sass: {                              
             dist: {                          
                 options: {                     
@@ -23,7 +11,6 @@ module.exports = function(grunt){
                 }
             }
         },
-
         watch: {
             js: {
               files: ['js/**/*.js'],
@@ -34,11 +21,8 @@ module.exports = function(grunt){
               tasks: ['sass'],        
             },
         }
-
-
     });
-
-    grunt.loadNpmTasks('grunt-contrib-concat');
+    
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.registerTask('default', ['watch']);
