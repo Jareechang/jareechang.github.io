a4f294b89da53a7c83539e2f2a8098323f0f415b
Jerry Chang <therealjerrycan@gmail.com>
2015-10-28 13:39:27 -0700

    initialized the new website

--- js/animations.js
+++ js/animations.js
@@ -1,4 +1,16 @@
 $(function(){
+    $enscholarDemo = $('#enscholar-demo');
+    $lettDemo = $('#lett-demo');
+    
+     $enscholarDemo.on('ended', function () {
+        this.load();      
+        $enscholarDemo[0].play();
+    });
+
+    $lettDemo.on('ended', function () {
+         this.load();      
+          $lettDemo[0].play();
+     });
 
     $(document).on('scroll' ,function(){
       // for handling the navbar banner color change
@@ -16,14 +28,27 @@ $(function(){
       //       $('.header').addClass('hidden');
       // }
 
-      if($(document).scrollTop() > 10){
+        // var targetHeight = $('#vision').outerHeight();
+        // console.log(targetHeight);
+        var scrollPosition = $(document).scrollTop(); 
+        var scrollPercent = scrollPosition / 300;        
+        
+        if(scrollPercent >= 0){
+            $('#vision').css('opacity', scrollPercent + 0.3);
+        }
+
+        if(scrollPosition > 10){
             $('.header').addClass('bg-emerald');
-            $('.header').removeClass('scroll-header-pad')
-      }else{
+            $('.header').removeClass('scroll-header-pad');
+            $('#header-title, #nav-right').removeClass('text-fade-white').addClass('text-white');            
+        }else{
             $('.header').addClass('scroll-header-pad')
             $('.header').removeClass('bg-emerald');
-      }
+            $('#header-title, #nav-right').removeClass('text-white').addClass('text-fade-white');
+        }
     })
+    
+
 
     $('#view-portfolio, #view-work').on('click', function(){
         $("html, body").animate({ scrollTop: 580 }, 600);        
